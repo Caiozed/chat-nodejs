d4dcda396e054c39182d7d77160de2f4a87375ea
Caio Neves <caio.nevesleite@gmail.com>
2019-06-05 01:00:39 -0300

    socketio test

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ var mongoUri =
   process.env.MONGOHQ_URL ||
   "mongodb://localhost:27017/chat";
 var app = express();
-var server = require("http").createServer(app);
-var websocket = require("socket.io").listen(server);
 
 // configure our server
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
@@ -177,13 +175,15 @@ MongoClient.connect(mongoUri, function(err, db) {
     });
   });
 
-  app.listen(port, function() {
+  let server = app.listen(port, function() {
     console.log("Server listening on port 3000");
   });
+
+  var websocket = socketio(server);
 
   websocket.on("connection", socket => {
     console.log("A client just joined on", socket.id);
   });
 
-  server.listen(process.env.PORT || 3000);
+  websocket.listen(3001);
 });
